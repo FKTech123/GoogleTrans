33cd8f8e6cb5ab0823535f6d5191e6df997e5eb5
Fathima K <116535666+FKTech123@users.noreply.github.com>
2024-06-29 21:36:59 +0530

    Create GoogleTranslation.py

---
+++ GoogleTranslation.py
@@ -0,0 +1,13 @@
+# from ast import With
+# from googletrans import Translator
+# translator = Translator()
+# translation = translator.translate('मेरा नाम फातिमा के है')
+# print(translation.text)
+
+# # Without .text in print statement
+# from googletrans import Translator
+# translator = Translator()
+# translation = translator.translate('मेरा नाम फातिमा के है')
+# print(translation)
+
+# To chaange the language which we want to translate
